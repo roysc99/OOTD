fc39f2d352d1e3dbf7cefff5ffc94108c5282346
William Yoon <89663919+wty5@users.noreply.github.com>
2025-01-07 18:05:14 -0500

    updated supabase api ketes

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/utils/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xxvipjhvzyibydwcqjgk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dmlwamh2enlpYnlkd2NxamdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwMDI5NjksImV4cCI6MjA1MDU3ODk2OX0.MJ33EM2-iW84fcmeCNS8c04QNRUXN-8gDgZFhRePyfs';
+const supabaseUrl = 'https://azbppiezjcqmxbtkofom.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6YnBwaWV6amNxbXhidGtvZm9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyOTA0ODEsImV4cCI6MjA1MTg2NjQ4MX0.5A-IOawMGqmkBkpkiL1kjQXcKRfRyb17AJm2Ln48wzM';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
